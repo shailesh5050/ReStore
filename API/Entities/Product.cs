43b7d47756333c5eb803d40edbeb4de3b7e54aa4
shailesh5050 <60616350+shailesh5050@users.noreply.github.com>
2022-02-27 21:43:09 +0530

    Section is done

---
+++ API/Entities/Product.cs
@@ -0,0 +1,29 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Quantity { get; set; }
+        
+        
+
+
+
+
+
+
+
+    }
+}
